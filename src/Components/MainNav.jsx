18309bc443402b3bf4454972bf34275bd1309bec
Inon Bauman <104355471+inonbauman@users.noreply.github.com>
2023-07-04 22:17:10 +0300

    Update MainNav.jsx

--- src/Components/MainNav.jsx
+++ src/Components/MainNav.jsx
@@ -9,7 +9,7 @@ function MainNav() {
       <img src={logo} className="App-logo w-10 rounded-full" alt="logo" />
       <ul>
         <li><a href="#home">עמוד הבית</a></li>
-        <li><a href="#about">עליי</a></li>
+        <li><a href="#aboutMe">עליי</a></li>
         <li><a href="#projects">הפרויקטים שלי</a></li>
       </ul>
       <a href={resume} rel="noreferrer" target="_blank" className=" bg-teal-500 rounded-full px-4 py-1 border border-teal-800 border border-4">קורות חיים</a>
